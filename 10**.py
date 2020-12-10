1babda14db4a391a1c2cdf6daa211c9299227dcc
hojmax <axelhojmark@gmail.com>
2020-12-10 11:38:34 +0100

    Working on readability

--- 10**.py
+++ 10**.py
@@ -2,37 +2,45 @@ import time
 from functools import reduce
 startTime = time.time()
 
-with open("/Users/axelhojmark/Dropbox/CODE/AOC/input.txt", 'r') as f:
-    lines = list(map(int,f.read().splitlines()))
-    lines.sort()
-    arrangements = [0] + lines + [lines[-1] + 3]
-    expendables = [[],[]]
-    for j in range(1,len(arrangements)-1):
-        if arrangements[j+1]-arrangements[j-1] <= 3:
-            expendables[0].append(j)
-        if j-2 >= 0 and arrangements[j+1]-arrangements[j-2] <= 3:
-            expendables[1].append([j-1,j])
-    singles = expendables[0]
+def getRemovables(lines):
+    removable = {"singles":[],"doubles":[]}
+    adapters = [0] + lines + [lines[-1] + 3]
+    for j in range(1,len(adapters)-1):
+        if adapters[j+1]-adapters[j-1] <= 3:
+            removable["singles"].append(j)
+        if j-2 >= 0 and adapters[j+1]-adapters[j-2] <= 3:
+            removable["doubles"].append([j-1,j])
+    return removable
+    
+def getPartitions(removable):
     i = 0
     partitions = []
-    while i < len(singles):
-        current = [singles[i]]
+    while i < len(removable["singles"]):
+        current = [removable["singles"][i]]
         j = i + 1
-        while j < len(singles) and singles[j]-current[-1] == 1:
-            current.append(singles[j])
+        while j < len(removable["singles"]) and removable["singles"][j]-current[-1] == 1:
+            current.append(removable["singles"][j])
             j += 1
         partitions.append(current)
         i = j
-    binaryComb = {1:2,2:3,3:5}
-    binaryParts = []
-    for e in partitions:
-        binaryParts.append(binaryComb[len(e)])
+    return partitions
+    
+with open("/Users/axelhojmark/Dropbox/CODE/AOC/input.txt", 'r') as f:
+    lines = list(map(int,f.read().splitlines()))
+    lines.sort()
+    removable = getRemovables(lines)
+    partitions = getPartitions(removable)
+    partitionPossibilities = {1: 2,
+                              2: 3,
+                              3: 5}
+    actualPossibilities = [partitionPossibilities[len(e)] for e in partitions]
     for i in range(len(partitions)):
-        for e in expendables[1]:
+        for e in removable["doubles"]:
             if min(e) > max(partitions[i]):
                 break
             if min(e) >= min(partitions[i]) and max(e) <= max(partitions[i]):
-                binaryParts[i] += 1
-    print(reduce(lambda x, y: x*y, binaryParts))
+                actualPossibilities[i] += 1
+    combinations = reduce(lambda x, y: x*y, actualPossibilities)
+    print(combinations)
 
 print("**** Runtime: {} seconds ****".format(round(time.time() - startTime, 2)))
